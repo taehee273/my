702dd8c4d6b7400cd59a1efea20aef0d69da2f2d
taehee273 <heehee_03@naver.com>
2024-09-13 00:54:19 +0900

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 4001;
+const port = 8001;
 
 app.use(cors({
     origin: '*',
